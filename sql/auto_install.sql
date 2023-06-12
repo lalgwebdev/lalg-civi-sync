ae1ec89cf9f379e89480934daf7371188461b789
maynardsmith <tony.maynard-smith@ntlworld.com>
2023-06-12 23:05:50 +0300

    Fixes. SearchKit works

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -36,16 +36,16 @@ SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE `lalg_cms_user` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique LalgCmsUser ID',
   `contact_id` int unsigned COMMENT 'FK to Contact',
-  `display_name` varchar(128) COMMENT 'Contact Display Name', 
+  `display_name` text COMMENT 'Contact Display Name',
   `user_id` int unsigned COMMENT 'Id of Drupal User',
-  `username` varchar(60) COMMENT 'Login username',
-  `email` varchar(254) COMMENT 'Email Address',
+  `username` text COMMENT 'Login username',
+  `email` text COMMENT 'Email Address',
   `last_login` timestamp COMMENT 'Last User Login',
   `last_access` timestamp COMMENT 'Last User Access',
   `group_id` int unsigned COMMENT 'Id of Drupal Group',
-  `group_name` varchar(254) COMMENT 'Drupal Group Name',
-  `group_role` varchar(254) COMMENT 'Drupal Group Role',
+  `group_name` text COMMENT 'Drupal Group Name',
+  `group_role` text COMMENT 'Drupal Group Role',
   PRIMARY KEY (`id`),
-  CONSTRAINT FK_lalg_cms_user_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`)
+  CONSTRAINT FK_lalg_cms_user_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE
 )
 ENGINE=InnoDB;
